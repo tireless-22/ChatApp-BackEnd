4ea7a9c9be25263bd50d9a3bb6949d9b98e07faf
karth <karthiknaveen22022002@gmail.com>
2022-08-03 13:15:45 +0530

    first ocmmit

---
+++ src/main/java/com/knk/disp.java
@@ -0,0 +1,13 @@
+package com.knk;
+
+// package check1;
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class disp extends HttpServlet{
+	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException {
+
+		res.getWriter().println("Hello");
+		}
+}
